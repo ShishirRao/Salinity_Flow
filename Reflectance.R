c21cf2ca27b9f55acf2b1306362c531eeba721cb
ShishirRao <shishir.t.rao@gmail.com>
2025-07-30 18:07:41 -0400

    Getting pixels from different sites and accumulating them in R. Got values for most of Agha sampling dates.

--- Reflectance.R
+++ Reflectance.R
@@ -7,6 +7,8 @@ library(ggrepel)
 
 setwd("E:/Shishir/FieldData/Analysis/Reflectance/")
 
+######################### Agha ###############
+
 # reflectance for dry season
 Agh_refl_dry = read.csv("L8_Agha_Dry_properties.csv",header=T)
 head(Agh_refl_dry)
@@ -19,8 +21,9 @@ ggplot(Agh_refl_dry,aes(y = avgRED, x = Date_interval_start,label = Date_interva
   theme(axis.text=element_text(size=10),
         axis.title=element_text(size=10,face="bold"))
 
-# only pixels classifed as water 1 and/or water 2 by DWSE
-Agh_refl_dry_class12 = read.csv("L8_Agha_Dry_properties_Class1_2Water.csv",header=T)
+# only pixels classifed as water 1 and/or water 2 by DWSE, and avg bright < 0.5
+#Agh_refl_dry_class12 = read.csv("L8_Agha_Dry_properties_Class1_2Water.csv",header=T)
+Agh_refl_dry_class12 = read.csv("L8_Agha_Dry_properties_Class1_2Water_v2.csv",header=T)
 head(Agh_refl_dry_class12)
 Agh_refl_dry_class12$Date_interval_start = ymd(stri_sub(Agh_refl_dry_class12$Date_interval_start,from = 1,to = 10))
 
@@ -52,10 +55,17 @@ ggplot(Agh_refl_wet,aes(y = avgRED, x = Date_interval_start))+geom_point()+
   theme(axis.text=element_text(size=12),
         axis.title=element_text(size=14,face="bold"))
 
-# merge the wet and dry season data
+
 # in dry season, avgRED values above 0.5 are faulty. Check why the cloud check is failing. 
-Agh_refl_dry = Agh_refl_dry %>% filter(avgRED<=0.5)
-Agh_refl = rbind(Agh_refl_dry,Agh_refl_wet %>% filter(Date_interval_start == ymd("2023-08-05")))
+# Cloud check failing can be prevented by checking avg brightness < 0.5
+# use class1_2 water instead. 
+# Agh_refl_dry = Agh_refl_dry %>% filter(avgRED<=0.5)
+
+
+# merge the wet and dry season data
+# Agh_refl = rbind(Agh_refl_dry,Agh_refl_wet %>% filter(Date_interval_start == ymd("2023-08-05")))
+Agh_refl = rbind(Agh_refl_dry_class12,Agh_refl_wet %>% filter(Date_interval_start == ymd("2023-08-05")))
+
 
 ggplot(Agh_refl,aes(y = avgRED, x = Date_interval_start))+geom_point()+
   geom_smooth() + ggtitle("all pixels_wet+dry season")+
@@ -95,4 +105,43 @@ ggplot(ssc_agh, aes(x = avgRED, y = log(ssc_mean))) +
   geom_point() +
   geom_smooth(method = "lm", formula = log(y) ~ x, color = "red")
   
+######################### Gangavali ###############
+
+# reflectance for dry season
+Gang_refl_dry = read.csv("L8_Gang_Dry_properties_Class1_2.csv",header=T)
+head(Gang_refl_dry)
+Gang_refl_dry$Date_interval_start = ymd(stri_sub(Gang_refl_dry$Date_interval_start,from = 1,to = 10))
+
+ggplot(Gang_refl_dry,aes(y = avgRED, x = Date_interval_start,label = Date_interval_start))+geom_text_repel()+
+  geom_point()+
+  geom_smooth() + ggtitle("all pixels_dry season")+  
+  scale_x_date(date_labels = "%b-%d",date_breaks = "16 day")+theme_bw()+
+  theme(axis.text=element_text(size=10),
+        axis.title=element_text(size=10,face="bold"))
+
+
+
+ssc_gang = ssc %>% filter(River == "Gangavali") %>% select(c("Sampling.Date","ScheduledDates","SSC..mg.l.","SamplingMonth"))
+
+#compute the median SSC for each sampling date
+ssc_gang =   ssc_gang %>%
+  dplyr::group_by(Sampling.Date) %>%
+  dplyr::summarize(ssc_mean = mean(SSC..mg.l., na.rm=TRUE))
+
+names(Gang_refl_dry)[1] = names(ssc_gang)[1]
+
+ssc_gang$Sampling.Date[ssc_gang$Sampling.Date == "2023-04-08"]
+
+
+#join ssc and reflectance data sets
+ssc_gang = left_join(ssc_gang,Gang_refl_dry)
+
+
+plot((ssc_agh$ssc_mean)~ssc_agh$avgRED)
+
+
+plot(log(ssc_agh$ssc_mean)~ssc_agh$avgRED)
+Agh_lm = lm(log(ssc_agh$ssc_mean)~ssc_agh$avgRED)
+
+abline(Agh_lm)
 
---
+++ ReflectenacePixelValues.R
@@ -0,0 +1,89 @@
+library(dplyr)
+library(ggplot2)
+library(lubridate)
+library(tidyr)
+library(stringr)
+library(ggrepel)
+library(stringi)
+
+setwd("E:/Shishir/FieldData/Analysis/Reflectance/")
+
+#Create a date series for 2023 spaced 8 days apart
+
+LandsatDates = data.frame("ImgDates" = seq(ymd("2023-01-09"),ymd("2023-12-27"), by = "8 days"))
+
+pix = read.csv("pixelValues_v2.csv",header=T)
+names(pix)
+head(pix)
+
+pix = pix %>% select("name","B2","B3","B4","B5","pixel_qa","date")
+names(pix) = c("site","Blue","Green","Red","NIR","pixel_qa","ImgDates")
+
+pix$ImgDates = ymd(stri_sub(pix$ImgDates,from = 1,to = 10))
+pix$River = stri_sub(pix$site,from = 1,to = 4)
+
+# Any values greater than 0.5 are cloud pixels that went undetected by qa bits
+pix = pix %>% filter(Red <= 0.5)
+
+pix = pix %>% group_by(site,ImgDates) %>% mutate(AvgRed = median(Red)) %>% select(site,ImgDates,River,AvgRed) 
+pix = pix %>% distinct()
+pix = pix %>% filter(River == "Agha")
+
+
+#These are the dates on which Agha_water_wet1 site has data compared to sampling site
+wet1dates = pix$date[pix$site == "Agha_water_wet1"][which(!(unique(pix$ImgDates[pix$site == "Agha_water_wet1"]) %in% unique(pix$ImgDates[pix$site == "Agha_water_dry"])) == TRUE)]
+wet2dates = pix$date[pix$site == "Agha_water_wet2"][which(!(unique(pix$ImgDates[pix$site == "Agha_water_wet2"]) %in% unique(pix$ImgDates[pix$site == "Agha_water_dry"])) == TRUE)]
+
+pix_wide = pix %>% spread(site,AvgRed)
+
+# combine the values for each site based on cloud free data availability
+# Use mostly Agha_water_dry for dry season
+pix_wide$AghaSSC = pix_wide$Agha_water_dry
+pix_wide$AghaSSC[pix_wide$ImgDates == ymd("2023-08-05")] = pix_wide$Agha_water_wet1[pix_wide$ImgDates == ymd("2023-08-05")]
+pix_wide$AghaSSC[pix_wide$ImgDates == ymd("2023-07-28")] = pix_wide$Agha_water_wet2[pix_wide$ImgDates == ymd("2023-07-28")]
+pix_wide$AghaSSC[pix_wide$ImgDates == ymd("2023-08-21")] = pix_wide$Agha_water_wet2[pix_wide$ImgDates == ymd("2023-08-21")]
+pix_wide = pix_wide %>% select(-c(Agha_water_dry,Agha_water_wet1,Agha_water_wet2))
+
+
+names(ssc)
+## Now get ssc values and compute the median SSC for each sampling date
+ssc_mean =   ssc %>% select(c("Sampling.Date","ScheduledDates","SSC..mg.l.","SamplingMonth","River")) 
+ssc_mean = ssc_mean %>% group_by(ScheduledDates,River) %>% mutate(ssc_mean = mean(SSC..mg.l.))
+ssc_mean = ssc_mean %>% select(-SSC..mg.l.) %>% distinct()
+names(ssc_mean)
+ssc_mean$logssc = log(ssc_mean$ssc_mean)
+#discard high SSC values from Oct and NOv
+ssc_mean = ssc_mean %>% filter(River == "Agha" & ScheduledDates < ymd("2023-10-16"))
+
+?distinct
+
+## 
+#refl = left_join(LandsatDates,ssc_mean)
+refl = left_join(LandsatDates,pix_wide)
+
+
+names(refl)
+names(ssc_mean)
+
+by <- join_by(River,closest(ImgDates >= Sampling.Date))
+refl = left_join(refl,ssc_mean,by)
+
+refl$dateDiff = as.numeric(refl$ImgDates- refl$Sampling.Date)
+
+refl = refl %>% filter(dateDiff <= 4)
+
+#### plotting ####
+plot((refl$ssc_mean)~refl$AghaSSC)
+
+
+plot(log(refl$ssc_mean)~refl$AghaSSC)
+Agh_lm = lm(log(refl$ssc_mean)~refl$AghaSSC)
+
+abline(Agh_lm)
+
+
+ggplot(ssc_agh, aes(x = avgRED, y = log(ssc_mean))) +
+  geom_point() +
+  geom_smooth(method = "lm", formula = log(y) ~ x, color = "red")
+
+
